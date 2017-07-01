1fa6af878cc13565495ad4b3a5a25f96919f2bd3
hibagus <git@bagus.my.id>
2017-07-01 21:33:44 +0700

    Add override method onActivityResult to handle feedback request from child activity. Change startActivity to startActivityForResult when calling a child activity

--- app/src/main/java/digitalquantuminc/inscribesecuresms/ActivityMain.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/ActivityMain.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,7 +22,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import digitalquantuminc.inscribesecuresms.DataType.TypeContact;
+import digitalquantuminc.inscribesecuresms.Development.ContactDummyData;
 import digitalquantuminc.inscribesecuresms.Intent.IntentString;
 import digitalquantuminc.inscribesecuresms.ListViewAdapter.contactListAdapter;
 import digitalquantuminc.inscribesecuresms.Repository.contactRepository;
@@ -34,19 +33,7 @@ import digitalquantuminc.inscribesecuresms.View.ViewSessionList;
 
 public class ActivityMain extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     // Global Variable for UX Binding
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private static final int DEVELOPMENT_MODE = 1;
 
     private ViewPager mViewPager;
@@ -68,13 +55,14 @@ public class ActivityMain extends AppCompatActivity {
 
         // Dummy data, only for development only.
         if (DEVELOPMENT_MODE == 1) {
-            ClearDB();
-            CreateDB();
-            LoadDummyData();
+            ContactDummyData.ClearDB(this);
+            ContactDummyData.CreateDB(this);
+            ContactDummyData.LoadDummyData(this);
         }
 
         // UX Layout Setup
         setupUXLayout();
+        //setButtonOnClickListener();
 
         // UI Component Binding
         //UIComponentBinding();
@@ -104,6 +92,24 @@ public class ActivityMain extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check request code send by child activity before it closes itself.
+        switch (requestCode) {
+            case IntentString.MainFeedbackCode_DoNothing: {
+                break;
+            }
+            case IntentString.MainFeedbackCode_RefreshContactList: {
+                LoadContactList(viewcontactslist.getList_contacts());
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 
     private void setupUXLayout() {
@@ -151,11 +157,6 @@ public class ActivityMain extends AppCompatActivity {
     }
 
 
-    //public void list_conversation_OnItemClick(AdapterView<?> parent, View view, int position, long id)
-    //{
-    //    TextView text_ContactPhoneNumber = view.findViewById(R.id.text_ContactPhoneNumber)
-    //}
-
     private void listview_contactList_onItemClick(AdapterView<?> parent, View view, int position, long id) {
         TextView textlist_ContactPhoneNumber = (TextView) view.findViewById(R.id.textlist_ContactPhoneNumber);
         ImageView imageView_ContactAccent = (ImageView) view.findViewById(R.id.imageView_ContactAccent);
@@ -163,45 +164,9 @@ public class ActivityMain extends AppCompatActivity {
         Intent objIntent = new Intent(getApplicationContext(), ActivityContactsDetail.class);
         objIntent.putExtra(IntentString.MainToContactsDetails_PhoneNum, ContactPhoneNumber);
         objIntent.putExtra(IntentString.MainToContactsDetails_ColorTheme, ((ColorDrawable) imageView_ContactAccent.getBackground()).getColor());
-        startActivity(objIntent);
+        startActivityForResult(objIntent, IntentString.MainFeedbackCode_RefreshContactList);
     }
 
-    public void LoadDummyData() {
-        TypeContact test1 = new TypeContact("081395141700", "Bagus Hanindhito", System.currentTimeMillis(), "00000000");
-        TypeContact test2 = new TypeContact("081321132456", "Fulan Bin Abi Fulan", System.currentTimeMillis(), "00000000");
-        TypeContact test3 = new TypeContact("085320456178", "Chobi Chocobi", System.currentTimeMillis(), "00000000");
-        TypeContact test4 = new TypeContact("089203040120", "Oryza Sativa", System.currentTimeMillis(), "00000000");
-        TypeContact test5 = new TypeContact("085320456179", "Cannabis Sativa", System.currentTimeMillis(), "00000000");
-        TypeContact test6 = new TypeContact("087482813212", "Aurelia Auritania", System.currentTimeMillis(), "00000000");
-        TypeContact test7 = new TypeContact("082145875521", "Fitria Ridayanti", System.currentTimeMillis(), "00000000");
-        TypeContact test8 = new TypeContact("087545221214", "Wisnu Wijayanto", System.currentTimeMillis(), "00000000");
-        TypeContact test9 = new TypeContact("025871422552", "Netizen Budiman", System.currentTimeMillis(), "00000000");
-        TypeContact test10 = new TypeContact("025478525224", "Mantan Terindah", System.currentTimeMillis(), "00000000");
-        TypeContact test11 = new TypeContact("025874568545", "Dia Yang Tersakiti", System.currentTimeMillis(), "00000000");
-        TypeContact test12 = new TypeContact("036587855457", "Maung Bandung", System.currentTimeMillis(), "00000000");
-        contactRepository contactRepo = new contactRepository(this);
-        contactRepo.insert(test1);
-        contactRepo.insert(test2);
-        contactRepo.insert(test3);
-        contactRepo.insert(test4);
-        contactRepo.insert(test5);
-        contactRepo.insert(test6);
-        contactRepo.insert(test7);
-        contactRepo.insert(test8);
-        contactRepo.insert(test9);
-        contactRepo.insert(test10);
-        contactRepo.insert(test11);
-        contactRepo.insert(test12);
-    }
 
-    public void ClearDB() {
-        contactRepository contactRepo = new contactRepository(this);
-        contactRepo.DropTable();
-    }
-
-    public void CreateDB() {
-        contactRepository contactRepo = new contactRepository(this);
-        contactRepo.CreateTable();
-    }
 
 }
