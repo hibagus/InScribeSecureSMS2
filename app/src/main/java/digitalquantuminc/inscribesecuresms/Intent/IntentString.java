6bbbb0ea9d71186a193ea6264bcd572968f99532
hibagus <git@bagus.my.id>
2017-07-20 09:19:40 +0700

    Define some new intentstring for child activity ActivitySessionDetail including MainToSessionDetails_PhoneNum, MainToSessionDetails_ColorTheme, and MainFeedbackCode_RefreshSessionList

--- app/src/main/java/digitalquantuminc/inscribesecuresms/Intent/IntentString.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/Intent/IntentString.java
@@ -8,6 +8,9 @@ package digitalquantuminc.inscribesecuresms.Intent;
 public class IntentString {
     public static final String MainToContactsDetails_PhoneNum = "ContactPhoneNum";
     public static final String MainToContactsDetails_ColorTheme = "ColorTheme";
+    public static final String MainToSessionDetails_PhoneNum = "PartnerNum";
+    public static final String MainToSessionDetails_ColorTheme = "ColorTheme";
     public static final int MainFeedbackCode_DoNothing = 0;
     public static final int MainFeedbackCode_RefreshContactList = 1;
+    public static final int MainFeedbackCode_RefreshSessionList = 2;
 }
