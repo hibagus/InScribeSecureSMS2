07cbe251f9c82fa7a93c576c0caa168d46cabca8
hibagus <git@bagus.my.id>
2017-07-20 16:22:23 +0700

    New Branch

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -960,14 +960,14 @@ public final class R {
         public static final int abc_shareactionprovider_share_with = 0x7f070010;
         public static final int abc_shareactionprovider_share_with_application = 0x7f070011;
         public static final int abc_toolbar_collapse_description = 0x7f070012;
-        public static final int appbar_scrolling_view_behavior = 0x7f070024;
-        public static final int bottom_sheet_behavior = 0x7f070025;
-        public static final int character_counter_pattern = 0x7f070026;
-        public static final int password_toggle_content_description = 0x7f070046;
-        public static final int path_password_eye = 0x7f070047;
-        public static final int path_password_eye_mask_strike_through = 0x7f070048;
-        public static final int path_password_eye_mask_visible = 0x7f070049;
-        public static final int path_password_strike_through = 0x7f07004a;
+        public static final int appbar_scrolling_view_behavior = 0x7f070026;
+        public static final int bottom_sheet_behavior = 0x7f070027;
+        public static final int character_counter_pattern = 0x7f070028;
+        public static final int password_toggle_content_description = 0x7f070048;
+        public static final int path_password_eye = 0x7f070049;
+        public static final int path_password_eye_mask_strike_through = 0x7f07004a;
+        public static final int path_password_eye_mask_visible = 0x7f07004b;
+        public static final int path_password_strike_through = 0x7f07004c;
         public static final int search_menu_title = 0x7f070013;
         public static final int status_bar_notification_info_overflow = 0x7f070014;
     }
