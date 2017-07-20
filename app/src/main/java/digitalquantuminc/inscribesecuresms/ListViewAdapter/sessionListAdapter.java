4e672ea83106c27d681a5fc61b2c1953246be79a
hibagus <git@bagus.my.id>
2017-07-20 09:20:32 +0700

    Implement Override Method getView for Session Item in the list

--- app/src/main/java/digitalquantuminc/inscribesecuresms/ListViewAdapter/sessionListAdapter.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/ListViewAdapter/sessionListAdapter.java
@@ -1,6 +1,7 @@
 package digitalquantuminc.inscribesecuresms.ListViewAdapter;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -68,16 +69,48 @@ public class sessionListAdapter extends BaseAdapter {
             holder = (sessionListViewHolder) convertView.getTag();
         }
 
-        // Prepare a HashMap to store single item Contact from Contact List
+        // Prepare a HashMap to store single item Contact from Session List
         HashMap<String, String> item = getItem(position);
 
-        // Set the list item based on the information from item Contact
+        // Set the list item based on the information from item Session
         holder.getTextlist_PartnerName().setText(item.get(TypeSession.KEY_name));
+        holder.getTextlist_PartnerNumber().setText(item.get(TypeSession.KEY_phone));
         holder.getTextlist_SessionHandshakeDate().setText(item.get(TypeSession.KEY_date));
+
+        // Elapsed Time
+        long mills = System.currentTimeMillis() - Long.parseLong(item.get(TypeSession.KEY_date));
+        int hours = (int) mills / (1000 * 60 * 60);
+        int mins = (int) mills % (1000 * 60 * 60);
+        int nummessage = Integer.valueOf(item.get(TypeSession.KEY_nummessage));
+
+        // Session Details
+        String textSessionDetails = "Session age is " + String.valueOf(hours) + " hour(s) " + String.valueOf(mins) + " minute(s) with " + String.valueOf(nummessage) + " message(s) processed.";
+        holder.getTextlist_SessionDetails().setText(textSessionDetails);
+
+        // Set Thumbnail Color
         int color = mColorGenerator.getColor(item.get(TypeSession.KEY_name));
         TextDrawable drawable = mDrawableBuilder.build(String.valueOf(item.get(TypeSession.KEY_name).charAt(0)), color);
         holder.getImageView_PartnerView().setImageDrawable(drawable);
-        //holder.getImageView_ContactAccent().setBackgroundColor(color);
+
+        // Set Color to Indicates Session Status
+        if (Integer.valueOf(item.get(TypeSession.KEY_valid)) == 1) {
+            holder.getImageView_SessionStatus().setBackgroundColor(ContextCompat.getColor(outer, R.color.colorDarkGreen));
+            // Set Color to Indicates Session Freshness / Stale
+            int session_freshness = hours * 5 + nummessage;
+            if (session_freshness < 25) {
+                holder.getImageView_SessionDuration().setBackgroundColor(ContextCompat.getColor(outer, R.color.colorDarkGreen));
+            } else if (session_freshness >= 25 && session_freshness <= 100) {
+                holder.getImageView_SessionDuration().setBackgroundColor(ContextCompat.getColor(outer, R.color.colorAmber));
+            } else {
+                holder.getImageView_SessionDuration().setBackgroundColor(ContextCompat.getColor(outer, R.color.colorDarkRed));
+            }
+        } else {
+            holder.getImageView_SessionStatus().setBackgroundColor(ContextCompat.getColor(outer, R.color.colorDarkRed));
+            holder.getImageView_SessionDuration().setBackgroundColor(ContextCompat.getColor(outer, R.color.colorDarkRed));
+        }
+
+
+
         // return the view
         return convertView;
     }
