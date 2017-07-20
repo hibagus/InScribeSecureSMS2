58ee21e08b038e06be6276e8575e12fb6c926bb6
hibagus <git@bagus.my.id>
2017-07-20 14:57:34 +0700

    Modify text_partnerRSAPubKey to text_PartnerRSAPubKey Modify how child activity should send intent to parent. * Modify method IntentFeedback(int FeedbackType) to IntentFeedback(int status, int FeedbackType) * Remove onBackPressed(); before IntentFeedback because it will cause NULL on the Intent (Intent is sent without the information of the sender) ! EVIL * The onBackPressed() is replaced by finish();

--- app/src/main/java/digitalquantuminc/inscribesecuresms/ChildrenActivity/ActivityContactsDetail.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/ChildrenActivity/ActivityContactsDetail.java
@@ -1,9 +1,11 @@
 package digitalquantuminc.inscribesecuresms.ChildrenActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +20,7 @@ import digitalquantuminc.inscribesecuresms.DataType.TypeContact;
 import digitalquantuminc.inscribesecuresms.Intent.IntentString;
 import digitalquantuminc.inscribesecuresms.R;
 import digitalquantuminc.inscribesecuresms.Repository.contactRepository;
+import digitalquantuminc.inscribesecuresms.Repository.sessionRepository;
 import digitalquantuminc.inscribesecuresms.UserInterface.UserInterfaceColor;
 
 public class ActivityContactsDetail extends AppCompatActivity {
@@ -26,7 +29,7 @@ public class ActivityContactsDetail extends AppCompatActivity {
     private TextView text_PartnerName;
     private TextView text_PartnerNumber;
     private TextView text_ContactAcquisitionDate;
-    private EditText text_partnerRSAPubKey;
+    private EditText text_PartnerRSAPubKey;
     private Button btn_DeleteContact;
 
     // Intent Variable
@@ -58,8 +61,7 @@ public class ActivityContactsDetail extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            IntentFeedback(IntentString.MainFeedbackCode_DoNothing);
+            IntentFeedback(RESULT_OK, IntentString.MainFeedbackCode_DoNothing);
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -69,7 +71,7 @@ public class ActivityContactsDetail extends AppCompatActivity {
         text_PartnerName = (TextView) findViewById(R.id.text_PartnerName);
         text_PartnerNumber = (TextView) findViewById(R.id.text_PartnerNumber);
         text_ContactAcquisitionDate = (TextView) findViewById(R.id.text_ContactAcquisitionDate);
-        text_partnerRSAPubKey = (EditText) findViewById(R.id.text_PartnerRSAPubKey);
+        text_PartnerRSAPubKey = (EditText) findViewById(R.id.text_PartnerRSAPubKey);
         btn_DeleteContact = (Button) findViewById(R.id.btn_DeleteContact);
         btn_DeleteContact.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,23 +98,25 @@ public class ActivityContactsDetail extends AppCompatActivity {
         text_PartnerName.setText(contact.getContact_name());
         text_PartnerNumber.setText(contact.getPhone_number());
         text_ContactAcquisitionDate.setText(new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z").format(new Date(contact.getAcquisition_date())));
-        text_partnerRSAPubKey.setText(contact.getRsa_publickey());
-        text_partnerRSAPubKey.setTextIsSelectable(true);
-        text_partnerRSAPubKey.setKeyListener(null);
+        text_PartnerRSAPubKey.setText(contact.getRsa_publickey());
+        text_PartnerRSAPubKey.setTextIsSelectable(true);
+        text_PartnerRSAPubKey.setKeyListener(null);
     }
 
-    protected void IntentFeedback(int FeedbackType) {
-        Intent intent = new Intent();
-        setResult(FeedbackType, intent);
+    protected void IntentFeedback(int status, int FeedbackType) {
+        Intent intent = getIntent();
+        intent.putExtra(IntentString.MainFeedBackCode, FeedbackType);
+        setResult(status, intent);
         finish();
     }
 
     private void btn_DeleteContact_onClick(View v) {
         String PhoneNumber = text_PartnerNumber.getText().toString();
         contactRepository repo = new contactRepository(this);
+        sessionRepository repo2 = new sessionRepository(this);
         repo.delete(PhoneNumber);
-        onBackPressed();
-        IntentFeedback(IntentString.MainFeedbackCode_RefreshContactList);
+        repo2.delete(PhoneNumber);
+        IntentFeedback(Activity.RESULT_OK, IntentString.MainFeedbackCode_RefreshBothContactandSessionList);
     }
 
 }
