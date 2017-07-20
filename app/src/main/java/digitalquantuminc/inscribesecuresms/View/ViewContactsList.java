2e9d4fd613f7ce762a4d0dea9c4a30626c57425d
hibagus <git@bagus.my.id>
2017-07-20 09:23:54 +0700

    Remove all of unnecessary Getter. Remove all of unnecessary UI Binding Variable and its binding during onCreateView.

--- app/src/main/java/digitalquantuminc/inscribesecuresms/View/ViewContactsList.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/View/ViewContactsList.java
@@ -2,10 +2,7 @@ package digitalquantuminc.inscribesecuresms.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import digitalquantuminc.inscribesecuresms.R;
 
@@ -17,11 +14,6 @@ import digitalquantuminc.inscribesecuresms.R;
 public class ViewContactsList extends Presenter {
     //region Global Variable
     private ListView list_contacts;
-    private TextView text_PartnerNumber;
-    private TextView text_PartnerName;
-    private TextView text_ContactAcquisitionDate;
-    private EditText text_partnerRSAPubKey;
-    private Button btn_DeleteContact;
 
     //endregion
     //region Constructor
@@ -34,37 +26,12 @@ public class ViewContactsList extends Presenter {
     @Override
     public void onCreateView() {
         list_contacts = (ListView) view.findViewById(R.id.list_contacts);
-        text_PartnerNumber = (TextView) view.findViewById(R.id.text_PartnerNumber);
-        text_PartnerName = (TextView) view.findViewById(R.id.text_PartnerName);
-        text_ContactAcquisitionDate = (TextView) view.findViewById(R.id.text_ContactAcquisitionDate);
-        text_partnerRSAPubKey = (EditText) view.findViewById(R.id.text_partnerRSAPubKey);
-        btn_DeleteContact = (Button) view.findViewById(R.id.btn_DeleteContact);
     }
 
     //endregion
     //region Getter
     public ListView getList_contacts() {
         return list_contacts;
-    }
-
-    public TextView getText_PartnerNumber() {
-        return text_PartnerNumber;
-    }
-
-    public TextView getText_PartnerName() {
-        return text_PartnerName;
-    }
-
-    public TextView getText_ContactAcquisitionDate() {
-        return text_ContactAcquisitionDate;
-    }
-
-    public EditText getText_partnerRSAPubKey() {
-        return text_partnerRSAPubKey;
-    }
-
-    public Button getBtn_DeleteContact() {
-        return btn_DeleteContact;
     }
     //endregion
 }
