90730f9528a052ed940edbaa22e8c0f24b9a5242
hibagus <git@bagus.my.id>
2017-07-21 10:29:56 +0700

    Add AutoInitialization Remove AutoIncrement on KEY_ID because we only need one. (ID=0)

--- app/src/main/java/digitalquantuminc/inscribesecuresms/DataBase/profileDBHelper.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/DataBase/profileDBHelper.java
@@ -1,5 +1,6 @@
 package digitalquantuminc.inscribesecuresms.DataBase;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -28,14 +29,27 @@ public class profileDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_TABLE_PROFILE = "CREATE TABLE " + TypeProfile.TABLE + "("
-                + TypeProfile.KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + TypeProfile.KEY_ID + " INTEGER, "
                 + TypeProfile.KEY_phone + " TEXT, "
                 + TypeProfile.KEY_name + " TEXT, "
                 + TypeProfile.KEY_date + " INTEGER, "
-                + TypeProfile.KEY_rsapub + "TEXT,"
-                + TypeProfile.KEY_rsapriv + "TEXT)";
+                + TypeProfile.KEY_rsapub + " TEXT, "
+                + TypeProfile.KEY_rsapriv + " TEXT, "
+                + TypeProfile.KEY_lastsync + " INTEGER)";
 
         db.execSQL(CREATE_TABLE_PROFILE);
+        // Prepare Values to be inserted
+        ContentValues values = new ContentValues();
+        values.put(TypeProfile.KEY_ID, TypeProfile.DEFAULTID);
+        values.put(TypeProfile.KEY_name, TypeProfile.DEFAULTNAME);
+        values.put(TypeProfile.KEY_phone, TypeProfile.DEFAULTPHONENUM);
+        values.put(TypeProfile.KEY_date, TypeProfile.DEFAULTGENERATEDDATE);
+        values.put(TypeProfile.KEY_rsapub, TypeProfile.DEFAULTRSAPUBKEY);
+        values.put(TypeProfile.KEY_rsapriv, TypeProfile.DEFAULTRSAPRIVKEY);
+        values.put(TypeProfile.KEY_lastsync, TypeProfile.DEFAULTLASTSYNC);
+
+        // Insert Default Value
+        db.insert(TypeProfile.TABLE, null, values);
     }
 
     @Override
