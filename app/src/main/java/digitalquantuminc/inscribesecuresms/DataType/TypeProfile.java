2c9f5963fb8437ddc91f6b9d78a91efc1a09ca75
hibagus <git@bagus.my.id>
2017-07-21 10:31:12 +0700

    Add some static variable to define default value. Add KEY_lastsync to indicate latest message sync from inbox.

--- app/src/main/java/digitalquantuminc/inscribesecuresms/DataType/TypeProfile.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/DataType/TypeProfile.java
@@ -7,6 +7,16 @@ package digitalquantuminc.inscribesecuresms.DataType;
  */
 
 public class TypeProfile {
+    //region Static Variable
+    public static final int DEFAULTID = 0;
+    public static final String DEFAULTNAME = "Fulan Bin Abi Fulan";
+    public static final String DEFAULTPHONENUM = "6281000000000";
+    public static final Long DEFAULTGENERATEDDATE = System.currentTimeMillis();
+    public static final String DEFAULTRSAPUBKEY = "00000RSAPUBNOTAVAILABLE";
+    public static final String DEFAULTRSAPRIVKEY = "00000RSAPRIVNOTAVAILABLE";
+    public static final Long DEFAULTLASTSYNC = Long.valueOf(0);
+    //endregion
+
     //region SQL Table Key
     public static final String TABLE = "profile";
     public static final String KEY_ID = "id";
@@ -15,6 +25,7 @@ public class TypeProfile {
     public static final String KEY_date = "generateddate";
     public static final String KEY_rsapub = "rsapubkey";
     public static final String KEY_rsapriv = "rsaprivkey";
+    public static final String KEY_lastsync = "lastsync";
     //endregion
     //region Global Variable
     private String phone_number;
@@ -22,6 +33,7 @@ public class TypeProfile {
     private long generated_date;
     private String rsa_publickey;
     private String rsa_privatekey;
+    private long lastsync;
 
     //endregion
     //region Constructor
@@ -29,12 +41,13 @@ public class TypeProfile {
 
     }
 
-    public TypeProfile(String phone_number, String name_self, long generated_date, String rsa_publickey, String rsa_privatekey) {
+    public TypeProfile(String phone_number, String name_self, long generated_date, String rsa_publickey, String rsa_privatekey, long lastsync) {
         this.phone_number = phone_number;
         this.name_self = name_self;
         this.generated_date = generated_date;
         this.rsa_publickey = rsa_publickey;
         this.rsa_privatekey = rsa_privatekey;
+        this.lastsync = lastsync;
     }
 
     //endregion
@@ -59,6 +72,10 @@ public class TypeProfile {
         this.rsa_privatekey = rsa_privatekey;
     }
 
+    public void setLastsync(long lastsync) {
+        this.lastsync = lastsync;
+    }
+
     //endregion
     //region Getter
     public long getGenerated_date() {
@@ -80,5 +97,10 @@ public class TypeProfile {
     public String getRsa_publickey() {
         return rsa_publickey;
     }
+
+    public long getLastsync() {
+        return lastsync;
+    }
+
     //endregion
 }
