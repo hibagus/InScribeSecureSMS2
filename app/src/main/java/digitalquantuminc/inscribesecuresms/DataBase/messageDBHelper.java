0ee0066b6744d91ebfcb4f381be93255bc70c6db
farizazmip <farizazmip@gmail.com>
2017-07-29 15:24:41 +0700

    Update messageDBHelper.java

--- app/src/main/java/digitalquantuminc/inscribesecuresms/DataBase/messageDBHelper.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/DataBase/messageDBHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import digitalquantuminc.inscribesecuresms.DataType.TypeMessage;
 
 /**
- * Created by Bagus Hanindhito on 24/07/2017.
+ * Created by Fariz Azmi Pratama on 24/07/2017.
  */
 
 public class messageDBHelper extends SQLiteOpenHelper {
