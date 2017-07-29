e17587ceb5d168faa412634cdfa2801d9003d87b
farizazmip <farizazmip@gmail.com>
2017-07-29 15:24:22 +0700

    Update contactDBHelper.java

--- app/src/main/java/digitalquantuminc/inscribesecuresms/DataBase/contactDBHelper.java
+++ app/src/main/java/digitalquantuminc/inscribesecuresms/DataBase/contactDBHelper.java
@@ -7,8 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import digitalquantuminc.inscribesecuresms.DataType.TypeContact;
 
 /**
- * Created by Bagus Hanindhito on 28/06/2017.
- * This class is handle SQLite Database for the Contact Database.
+ * Created by Fariz Azmi Pratama on 28/06/2017.
  */
 
 public class contactDBHelper extends SQLiteOpenHelper {
